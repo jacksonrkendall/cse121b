30ba1a7ebe64e0d0e7b9539e401091c6f0a6acd0
Jackson Kendall <jackson.kendall@icloud.com>
2024-01-18 16:32:23 -0700

    doing homework

---
+++ w02-task/scripts/w02-task.js
@@ -0,0 +1,31 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+const fullName = 'Jackson Kendall';
+const currentYear = new Date().currentYear();
+const profilePicture = 'images/profile pic.jpg';
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('year');
+/* Step 3 - Element Variables */
+
+
+
+
+
+/* Step 4 - Adding Content */
+
+
+
+
+
+
+/* Step 5 - Array */
+
+
+
+
+
+
