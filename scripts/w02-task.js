011f5bc8c741ea014cb0f0306130e4d61c2d7d2b
Jackson Kendall <jackson.kendall@icloud.com>
2024-01-18 23:09:41 -0700

    debugged w03-task.js

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -14,18 +14,18 @@ const yearElement = document.querySelector('#year');
 const imageElement = document.querySelector('picture img');
 
 /* Step 4 - Adding Content */
-nameElement.innerHTML = '<strong>${fullName}</strong>';
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
 yearElement.textContent = currentYear;
 imageElement.setAttribute('src', profilePicture);
-imageElement.setAttribute('alt', 'Profile image of ${fullName}');
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
 
 /* Step 5 - Array */
 let favoriteFoods = ['lasagna', 'pasta', 'dutch babies', 'burritos'];
 foodElement.innerHTML = favoriteFoods;
 const newFavoriteFood = 'sushi';
 favoriteFoods.push(newFavoriteFood);
-foodElement.innerHTML += '<br>$favoriteFoods';
+foodElement.innerHTML += `<br>${favoriteFoods}`;
 favoriteFoods.shift();
-foodElement.innerHTML += '<br>$favoriteFoods';
+foodElement.innerHTML += `<br>${favoriteFoods}`;
 favoriteFoods.pop();
-foodElement.innerHTML += '<br>$favoriteFoods';
+foodElement.innerHTML += `<br>${favoriteFoods}`;
