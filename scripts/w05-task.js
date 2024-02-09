c7df6206790fee8101d21c0df2d1b9292b823609
Jackson Kendall <jackson.kendall@icloud.com>
2024-02-08 19:20:31 -0700

    starting wk5 task

---
+++ scripts/w05-task.js
@@ -0,0 +1,22 @@
+/* W05: Programming Tasks */
+
+/* Declare and initialize global variables */
+const templesElement = document.getElementById('temples');
+let templeList = [];
+/* async displayTemples Function */
+const displayTemples = (temples) => {
+temples.forEach(temple )
+};
+
+/* async getTemples Function using fetch()*/
+
+
+/* reset Function */
+
+
+/* filterTemples Function */
+
+
+getTemples();
+
+/* Event Listener */
